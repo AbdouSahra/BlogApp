5ce4bd44fba2eadfaa79930e2b43e050081e1c95
abdo <abdo@abdo.com>
2021-02-18 17:31:18 +0100

    aa

--- users/models.py
+++ users/models.py
@@ -5,7 +5,7 @@ from PIL import Image
 class Profile(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     bio = models.TextField()
-    picture = models.ImageField(upload_to='profile_pics')
+    picture = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
     def __str__(self):
         return str(self.user)
